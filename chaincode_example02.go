8d4f9ce16d2c94a88e1d8756b6ba6c1cf1efe934
Anton Kovernyk <akover@softserveinc.com>
2017-04-21 15:17:06 +0300

    new commit3

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -8,6 +8,10 @@ import (
 
 var logger = shim.NewLogger("CLDChaincode")
 
+
+type  SimpleChaincode struct {
+}
+
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("ex02 Init")
 	//_, args := stub.GetFunctionAndParameters()
